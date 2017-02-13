b4a22dfe4f92a3b18ce90fcbe6eb4a5bce061fd5
fikr4n <fikr4n@gmail.com>
2017-02-13 23:53:24 +0700

    Convert project to gradle

---
+++ app/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        SalatExample.main(args);
+        QiblaExample.main(args);
+        HijriExample.main(args);
+    }
+}
